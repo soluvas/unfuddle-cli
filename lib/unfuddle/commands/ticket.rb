8a3d75f3a6119c4beefa070a9d936cfa784415b4
Hendy Irawan <hendy@soluvas.com>
2012-02-20 10:12:35 +0700

    Linux "Growl" notification using `notify-send`. Fixed #3.

--- lib/unfuddle/commands/ticket.rb
+++ lib/unfuddle/commands/ticket.rb
@@ -7,6 +7,9 @@ class Unfuddle::Command::TicketCommand < Unfuddle::Command
     unless tickets.empty?
       display 'Tickets:'
       tickets.each { |p| display "##{p.id}: #{p.summary}" }
+      system '/usr/bin/notify-send',
+        '--icon=/usr/share/icons/gnome/48x48/status/info.png',
+        "Unfuddle: #{tickets.count} tickets found"
     else
       display 'You dont have any tickets yet'
     end
@@ -16,13 +19,17 @@ class Unfuddle::Command::TicketCommand < Unfuddle::Command
   def create
     display 'Create a new ticket'
     display "Site: #{Unfuddle::Resources::Ticket.site}"
-    repo = Unfuddle::Resources::Ticket.create(
+    ticket = Unfuddle::Resources::Ticket.create(
       :ticket => {
         :summary => ask('Summary', :required => true),
         :description => ask('Description', :required => false),
         :priority => ask('Priority (1-5)', :required => true)
       }
     )
+    display "Ticket created: ##{ticket.id} - #{ticket.summary}"
+    system '/usr/bin/notify-send',
+      '--icon=/usr/share/icons/gnome/48x48/actions/document-new.png',
+      "Unfudde Ticket created: ##{ticket.id} - #{ticket.summary}"
   end
   
 end
